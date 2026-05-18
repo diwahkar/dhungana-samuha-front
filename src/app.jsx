05b7fc60d0288dea2f52c9a7c883fdf8d2e50a0c
Diwakar Dhungana <diwahkar@gmail.com>
2026-05-18 09:35:27 +0545

    new routes added

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,27 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from 'components/Layout'
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Members from 'pages/members';
-import Loans from 'pages/loans';
-import MonthlyAmounts from 'pages/monthlyAmounts';
+import Login from 'pages/Login'
+import Dashboard from 'pages/Dashboard'
+import Members from 'pages/Members'
+import MemberProfile from 'pages/MemberProfile'
+import Savings from 'pages/Savings'
+import Loans from 'pages/Loans'
+import Interests from 'pages/Interests'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <Link to='/members'>Members</Link> |{' '}
-        <Link to='/loans'>Loans</Link> |{' '}
-        <Link to='/monthlyAmounts'>Monthly Amounts</Link>
-      </nav>
+    <Routes>
 
-      <Routes>
-        <Route path='/members' element={<Members />} />
-        <Route path='/loans' element={<Loans />} />
-        <Route path='/monthlyAmounts' element={<MonthlyAmounts />} />
-      </Routes>
-    </BrowserRouter>
-  );
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login /> } />
+      <Route path="/dashboard" element={<Layout> <Dashboard /> </Layout>} />
+      <Route path="/members" element={<Layout> <Members /> </Layout>} />
+      <Route path="/members/:id" element={<Layout> <MemberProfile /> </Layout>} />
+      <Route path="/savings" element={<Layout> <Savings /> </Layout>} />
+      <Route path="/loans" element={<Layout> <Loans /> </Layout>} />
+      <Route path="/interests" element={<Layout> <Interests /> </Layout>} />
+
+    </Routes>
+  )
 }
